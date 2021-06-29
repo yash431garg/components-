e7f88cfb79ec8313f467cfcb780cf8246edabfea
Yash Garg <yash431garg@gmail.com>
2021-06-29 21:18:58 +0530

    Design Upgrades adn props added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import Card from "./components/Card/Card";
 function App() {
   return (
     <div className="App">
-      <Card />
-      <Card />
-      <Card />
+      <Card
+        img={
+          "https://www.viewbug.com/media/mediafiles/2016/04/30/65766312_large1300.jpg"
+        }
+      />
+      <Card img="https://innovationorigins.com/app/uploads/2018/10/20130523_180620-1-1004x631.jpg" />
+      <Card img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSE7MWzTQxC5tk_cWlpixi3j1ub0jM7SgbrdFW2iWpz-cfbfB7bVv0zIupljr1R6Q8YDkc&usqp=CAU" />
     </div>
   );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,13 +3,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectCard, setCardClicked } from "../../features/cardSlice";
 import "./Card.css";
 
-function Card() {
+function Card(props) {
   const isCardClicked = useSelector(selectCard);
   console.log(isCardClicked);
   const dispatch = useDispatch();
   return (
     <div>
-      {isCardClicked ? (
+      {false ? (
         "Cards are clicked"
       ) : (
         <div
@@ -18,19 +18,17 @@ function Card() {
             dispatch(setCardClicked(true));
           }}
         >
-          <div className="cardTop">
-            <i className="fas fa-chevron-left"></i>
-            <i class="fas fa-cog"></i>
+          <div>
+            <div className="cardTop">
+              <i className="fas fa-chevron-left"></i>
+              <i class="fas fa-cog"></i>
+            </div>
+            <hr className="hrElement" />
           </div>
           <div className="cardImage">
-            <img
-              src="https://picsum.photos/seed/picsum/200/300/ff"
-              alt="nature images"
-            />
+            <img src={props.img} alt="nature images" />
           </div>
-          <div className="cardBottom">
-            <hr className="hrElement" />I am the card cardBottom
-          </div>
+          <div className="cardBottom">I am the card cardBottom</div>
         </div>
       )}
     </div>
